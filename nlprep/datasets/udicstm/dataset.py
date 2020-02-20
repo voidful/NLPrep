dfe3df5a361beec3e4eb72fd12373d5218fd35c1
voidful <voidful.stack@gmail.com>
2020-02-21 00:27:37 +0800

    fix udicstm tag error

--- nlprep/datasets/udicstm/dataset.py
+++ nlprep/datasets/udicstm/dataset.py
@@ -10,10 +10,10 @@ def toMiddleFormat(paths):
     dataset = MiddleFormat()
     for path in paths:
         with open(path, encoding='utf8') as f:
-            if 'pos' in path:
-                sentiment = "positive"
-            else:
+            if "失望" in f.readline():
                 sentiment = "negative"
+            else:
+                sentiment = "positive"
             for i in list(f.readlines()):
                 dataset.add_data(i.strip(), sentiment)
     return dataset
