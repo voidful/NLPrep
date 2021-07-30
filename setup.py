df6f76b72aa93a16f0dc483f5515eb1b5248d4da
voidful <voidful.stack@gmail.com>
2021-07-30 23:14:25 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nlprep',
-    version='0.2.00',
+    version='0.2.01',
     description='Download and pre-processing data for nlp tasks',
     url='https://github.com/voidful/nlprep',
     author='Voidful',
